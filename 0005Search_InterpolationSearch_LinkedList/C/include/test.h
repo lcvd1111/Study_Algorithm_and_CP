5016a2450e1c92c6b5700a76f28cb15484157628
lcvd1111 <lcvd1111@gmail.com>
2023-12-04 08:59:10 +0900

    .

---
+++ 0005Search_InterpolationSearch_LinkedList/C/include/test.h
@@ -0,0 +1,8 @@
+#ifndef __TEST_HEADER__
+
+#include "common.h"
+
+int UnitTest_RandomList(void);
+int UnitTest_InterpolationSearch(void);
+
+#endif
---
+++ 0005Search_InterpolationSearch_LinkedList/C/src/test.c
@@ -0,0 +1,12 @@
+#include "test.h"
+#include "search.h"
+
+int UnitTest_RandomList(void)
+{
+	return 0;
+}
+
+int UnitTest_InterpolationSearch(void)
+{
+	return 0;
+}
