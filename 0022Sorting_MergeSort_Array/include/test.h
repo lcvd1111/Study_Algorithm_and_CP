12ea6a6e4778278f06831ba8308e76dd12431015
lcvd1111 <lcvd1111@gmail.com>
2024-03-09 21:27:02 +0900

    .

---
+++ 0022Sorting_MergeSort_Array/include/test.h
@@ -0,0 +1,11 @@
+#ifndef __UNIT_TEST_HEADER__
+#define __UNIT_TEST_HEADER__
+
+#include "common.h"
+#include "array.h"
+
+int UnitTest_Array(void);
+int UnitTest_Sort(void);
+int UnitTest(void);
+
+#endif
---
+++ 0022Sorting_MergeSort_Array/src/test.c
@@ -0,0 +1,33 @@
+#include "test.h"
+
+int UnitTest_Array(void)
+{
+	ARRAY testArray;
+	ARRAY_METHOD_CONSTRUCTOR(&testArray, 100);
+	testArray.Fill_Random(&testArray);
+	//testArray.Print(&testArray);
+	ARRAY_METHOD_DESTRUCTOR(&testArray);
+	return 0;
+}
+
+int UnitTest_Sort(void)
+{
+	return 0;
+}
+
+int UnitTest(void)
+{
+	if (UnitTest_Array()){
+		printf(COLOR_BLACK "Unit Test for Array: " COLOR_RED "FAIL\n" COLOR_BLACK);
+		return -1;
+	}
+	printf(COLOR_BLACK "Unit Test for Array: " COLOR_GREEN "SUCCESS\n" COLOR_BLACK);
+
+	if (UnitTest_Sort()){
+		printf(COLOR_BLACK "Unit Test for Merge Sort on array: " COLOR_RED "FAIL\n" COLOR_BLACK);
+		return -2;
+	}
+	printf(COLOR_BLACK "Unit Test for Merge Sort on array: " COLOR_GREEN "SUCCESS\n" COLOR_BLACK);
+
+	return 0;
+}
